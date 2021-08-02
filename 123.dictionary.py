abd311456fe25b2217f85cf3caa2c2a3dab63e16
BHAVYA TYAGI <72131872+Bhavya1912@users.noreply.github.com>
2021-08-02 23:33:08 +0530

    Update 123.dictionary.py

--- 123.dictionary.py
+++ 123.dictionary.py
@@ -8,7 +8,7 @@ def translate(word):
     if word in data:
         return data[word]
     elif word.title() in data:
-        return data[word.title()]
+        return data[word.title()] 
     elif word.upper() in data:
         return data[word.upper()]
     elif len(get_close_matches(word , data.keys())) > 0 :
